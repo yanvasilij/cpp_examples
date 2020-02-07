34c7a4abb45f059bde9ba72d00933e1c99aaf09d
yanikeev-vs <yanikeev-vs@nefteavtomatika.ru>
2020-02-07 10:54:57 +0500

    Переделал отправку данных по i2c

--- i2c/main.cpp
+++ i2c/main.cpp
@@ -5,6 +5,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <string.h>
 
 int  I2cWrite_(int fd, uint8_t Address , uint8_t *pBuffer, uint32_t NbData)
 {
@@ -24,22 +25,50 @@ int  I2cWrite_(int fd, uint8_t Address , uint8_t *pBuffer, uint32_t NbData)
     return status;
 }
 
+void testI2c (int fd)
+{
+    struct dataBuf {
+        i2c_send_t header;
+        unsigned char data[256];
+    };
+
+    struct dataBuf dev_data;
+
+    memset(dev_data.data, 0, sizeof(dev_data.data));
+    dev_data.header.slave.addr = 0x40; // address of factory config information
+    dev_data.header.slave.fmt = I2C_ADDRFMT_7BIT;
+    dev_data.header.len = 2; //  1
+    dev_data.header.stop = 1;
+    dev_data.data[0] = 0x20; //register address
+    dev_data.data[1] = 143; //the data to write
+
+    devctl(fd, DCMD_I2C_SEND, &dev_data, sizeof(dev_data), NULL );
+}
+
 int main(int argc, char **argv)
 {
     int i2ch;
     i2c_driver_info_t i2c_driver_info;
-    i2c_speed_mode_t speed_mode;
+    i2c_speed_mode_t speed_mode = I2C_SPEED_STANDARD;
     uint8_t send_str[2] = {'1', '2'};
 
     printf("i2c test started!\n");
 
+
     i2ch = open("/dev/i2c0", O_RDWR);
 
+    devctl(i2ch, DCMD_I2C_SET_BUS_SPEED, &speed_mode, sizeof(i2c_speed_mode_t), NULL );
+
+    devctl(i2ch, DCMD_I2C_DRIVER_INFO, &i2c_driver_info, sizeof(i2c_driver_info), NULL );
+    printf("addr_mode %d speed_mode %d\n", i2c_driver_info.addr_mode, i2c_driver_info.speed_mode);
 
 
     while (1)
     {
         I2cWrite_(i2ch, 0x00, send_str, 2);
+        //testI2c(i2ch);
+        devctl(i2ch, DCMD_I2C_DRIVER_INFO, &i2c_driver_info, sizeof(i2c_driver_info), NULL );
+        printf("addr_mode %d speed_mode %d\n", i2c_driver_info.addr_mode, i2c_driver_info.speed_mode);
         sleep(1);
     }
 
