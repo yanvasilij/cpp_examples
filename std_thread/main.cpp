316d124aa672dcb4838c18d9e7ef90d3f666e772
yanikeev-vs <yanikeev-vs@nefteavtomatika.ru>
2020-06-02 17:24:07 +0500

    Добавил пример работы с qnx таймером

--- std_thread/main.cpp
+++ std_thread/main.cpp
@@ -1,6 +1,18 @@
 #include <thread>
 #include <iostream>
 #include <chrono>
+#include <string>
+#include <csignal>
+
+namespace
+{
+    volatile std::sig_atomic_t gSignalStatus;
+}
+
+void signal_handler(int signal)
+{
+    gSignalStatus = signal;
+}
 
 class ThreadTest
 {
@@ -8,17 +20,19 @@ class ThreadTest
 
     void thread1 ()
     {
-        while (true)
+        std::signal(SIGINT, signal_handler);
+        while (gSignalStatus == 0)
         {
             std::this_thread::sleep_for(std::chrono::seconds(1));
-            std::cout << output << std::endl;
+            std::cout << output << "signal value: " << gSignalStatus << std::endl;
         }
+        std::cout << "thread1 end " << std::endl;
     }
 
     void thread2 ()
     {
         std::this_thread::sleep_for(std::chrono::seconds(5));
-        output = "New string value";
+        output = "New string value" + std::to_string(10);
     }
 
 public:
@@ -26,6 +40,7 @@ public:
 
     void start (void)
     {
+
         std::thread t2 (&ThreadTest::thread2, this);
         t2.detach();
 
